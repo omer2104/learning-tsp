03b1711d4ad78f9960179e98776d3fff2ae76d48
Omer Saar <omersaar2104@gmail.com>
2025-02-15 21:53:01 +0200

    torch upgrade code modifications

--- utils/functions.py
+++ utils/functions.py
@@ -27,7 +27,7 @@ def load_problem(name):
 
 
 def torch_load_cpu(load_path):
-    return torch.load(load_path, map_location=lambda storage, loc: storage)  # Load on CPU
+    return torch.load(load_path, map_location=lambda storage, loc: storage, weights_only=False)  # Load on CPU
 
 
 def move_to(var, device):
@@ -47,7 +47,7 @@ def _load_model_file(load_path, model):
         os.path.join(
             os.getcwd(),
             load_path
-        ), map_location=lambda storage, loc: storage)
+        ), map_location=lambda storage, loc: storage, weights_only=False)
 
     if isinstance(load_data, dict):
         load_optimizer_state_dict = load_data.get('optimizer', None)
